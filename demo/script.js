96932f90b4a6ae98ecd3368f45d13ca40b309ad1
pauldubois98 <pauldubois98@gmail.com>
2023-06-14 11:52:57 +0200

    GUI logic

--- demo/script.js
+++ demo/script.js
@@ -1,9 +1,12 @@
+var ctx = intensity.getContext('2d');
 var PTS = [];
+var DOWN = false;
+const WIDTH = intensity.width;
+const HEIGHT = intensity.height;
+
 
 function clear() {
-    if (!EXPORT) {
-        ctx.clearRect(0, 0, intensity.width, intensity.height);
-    }
+    ctx.clearRect(0, 0, intensity.width, intensity.height);
 }
 
 function add_point(x, y) {
@@ -24,7 +27,7 @@ function add_point(x, y) {
         }
     }
 }
-function draw_function() {
+function draw_intensity() {
     ctx.beginPath();
     ctx.strokeStyle = "black";
     ctx.lineWidth = 5;
@@ -36,3 +39,60 @@ function draw_function() {
     }
     ctx.stroke();
 }
+
+
+// user interaction
+intensity.addEventListener('mouseup', function (e) {
+    var rect = intensity.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    DOWN = false;
+    if(PTS.length === 1) {
+        PTS.push({ x: Math.round(x)+1, y: y });
+    }
+    add_point(x, y);
+    draw_intensity();
+});
+intensity.addEventListener('mouseleave', function (e) {
+    if (DOWN) {
+        var rect = intensity.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        add_point(Math.max(0, Math.min(x, WIDTH)), Math.max(0, Math.min(y, HEIGHT)));
+    }
+    DOWN = false;
+    draw_intensity();
+});
+intensity.addEventListener('mouseenter', function (e) {
+    if (e.buttons != 0) {
+        var rect = intensity.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        if (PTS.length === 0) {
+            PTS.push({ x: Math.round(x), y: y });
+        } else {
+            add_point(Math.max(0, Math.min(x, WIDTH)), Math.max(0, Math.min(y, HEIGHT)));
+        }
+        DOWN = true;
+        draw_intensity();
+    }
+});
+intensity.addEventListener('mousedown', function (e) {
+    var rect = intensity.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    DOWN = true;
+    PTS = [{ x: Math.round(x), y: y }];
+    BOXES = [];
+    clear();
+    draw_intensity();
+});
+intensity.addEventListener('mousemove', function (e) {
+    if (DOWN) {
+        var rect = intensity.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        add_point(x, y);
+        draw_intensity();
+    }
+});
