4b18c357faa6e6c02163268e17e5cde9cd494135
pauldubois98 <pauldubois98@gmail.com>
2023-06-16 15:25:38 +0200

    split to intervals

--- demo/script.js
+++ demo/script.js
@@ -13,6 +13,30 @@ function get_bounds() {
         bounds = [PTS[0].x, PTS[PTS.length-1].x];
     }
 }
+function split_to_intervals() {
+    if (PTS.length === 0) {
+        // console.log("No points");
+        intervals_bounds = [];
+        intervals_values = [];
+        return;
+    }
+    get_bounds();
+    var nb_intervals = Number(range_intervals.value);
+    var range = bounds[1] - bounds[0];
+    var interval_size = range / nb_intervals;
+    var current = bounds[0];
+    intervals_bounds = [current];
+    intervals_values = [];
+    for (var i = 0; i < nb_intervals; i++) {
+        var middle = current + interval_size / 2;
+        var middle_index = Math.round(middle)-bounds[0];
+        var middle_value = PTS[middle_index].y;
+        var next = current + interval_size;
+        intervals_bounds.push(next);
+        intervals_values.push(middle_value);
+        current = next;
+    }
+}
 
 function draw_all() {
     clear();
