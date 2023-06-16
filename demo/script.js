08fe306db263856d55741ce83b75143ee3befaa5
pauldubois98 <pauldubois98@gmail.com>
2023-06-16 13:14:04 +0200

    bounds

--- demo/script.js
+++ demo/script.js
@@ -1,13 +1,22 @@
 var nb_bixels = 1;
-var x_min = 0;
-var y_min = 0;
+var bounds = [];
 var intervals_bounds = [];
 var intervals_values = [];
 var v_max_per_bixel = 1;
 var power = 100;
 
+function get_bounds() {
+    if (PTS.length === 0) {
+        console.log("No points");
+        bounds = [];
+    } else {
+        bounds = [PTS[0].x, PTS[PTS.length-1].x];
+    }
+}
+
 function draw_all() {
     clear();
     draw_intensity();
-
+    get_bounds();
+    
 }
